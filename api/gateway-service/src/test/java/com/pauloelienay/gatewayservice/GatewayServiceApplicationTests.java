2c22fbb8ef95f5890ddf222e65ea54a9eb87e9b3
Paulo Elienay <paulo@pauloelienay.com>
2021-09-08 02:12:05 -0300

    chore(start.spring): generates `gateway-service`

---
+++ api/gateway-service/src/test/java/com/pauloelienay/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloelienay.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
